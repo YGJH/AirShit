5ebe6b23d1bb2a839256868c12d9878f3acb9850
YGJH <jihuty@gmail.com>
2025-04-23 00:24:57 +0800

    update

---
+++ bypass.cmd
@@ -0,0 +1,4 @@
+REM Save this as run‑bypass.cmd next to your .ps1
+@echo off
+REM Launch PowerShell without loading your profile and ignore the policy
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
