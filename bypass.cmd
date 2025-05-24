51b2b23ecc078a11e41aaf481e5cae6bb3b6889c
YGJH <jihuty@gmail.com>
2025-05-25 02:11:22 +0800

    tt

--- bypass.cmd
+++ bypass.cmd
@@ -4,3 +4,7 @@ REM Launch PowerShell without loading your profile and ignore the policy
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "build.ps1" %*
 
 @REM Set-ExecutionPolicy Unrestricted -Scope Process -Force
+
+WARNING: A restricted method in java.lang.System has been called
+WARNING: java.lang.System::load has been called by com.sun.glass.utils.NativeLibLoader in module javafx.graphics (file:/C:/Users/kkasdasd/.m2/repository/org/openjfx/javafx-graphics/20/javafx-graphics-20-win.jar)
+WARNING: Use --enable-native-access=javafx.graphics to avoid a warning for callers in this module
