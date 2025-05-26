7b009c35182c4df8e87e98cc6304c7dc5aaeae2f
YGJH <jihuty@gmail.com>
2025-05-26 18:17:04 +0800

    Update thread pool initialization in FileReceiver and FileSender; revert to dynamic thread count based on available processors

--- src/main/java/AirShit/FileReceiver.java
+++ src/main/java/AirShit/FileReceiver.java
@@ -20,8 +20,8 @@ import AirShit.ui.FileChooserDialog;
 public class FileReceiver {
 
     public int port;
-    // private final int ITHREADS = Runtime.getRuntime().availableProcessors();
-    private final int ITHREADS = 1000;
+    private final int ITHREADS = Runtime.getRuntime().availableProcessors() * 4;
+    // private final int ITHREADS = 1<<30;
     private File selectedSaveDirectory; // 確保這是 FileReceiver 的成員變數
     // These are now set per handshake
     // public String currentSenderName;
